3ec3a46cff155edc861d007e0c976948a1f7862a
Sug1re <sugirex@icloud.com>
2025-01-29 14:35:35 +0900

    zodを使ってバリデーションチェックを可能

--- components/component/Form.tsx
+++ components/component/Form.tsx
@@ -22,24 +22,42 @@ const formSchema = z.object({
   initialSetupCosts: z.number().min(1, "初期費用を選択してください。"),
   tuitionFee: z.number().min(1, "授業料を選択してください。"),
   testFee: z.number().min(1, "受験料を選択してください。"),
-  schooling: z.enum(["true", "false"], {
-    errorMap: () => ({ message: "スクーリングの有無を選択してください。" }),
-  }),
-  movingOutsideThePrefecture: z.enum(["true", "false"], {
-    errorMap: () => ({ message: "県外移動の有無を選択してください。" }),
-  }),
-  commutingStyle: z.enum(["通学", "オンライン"], {
-    errorMap: () => ({ message: "通学形態を選択してください。" }),
-  }),
-  highSchool: z.enum(["通信制高等学校", "サポート校"], {
-    errorMap: () => ({ message: "学校の種類を選択してください。" }),
-  }),
-  attendanceFrequency: z.enum(
-    ["週1", "週2", "週3", "週4", "週5", "オンライン", "自由"],
-    {
-      errorMap: () => ({ message: "登校頻度を選択してください。" }),
-    }
-  ),
+  schooling: z
+    .string()
+    .refine((val) => val !== "", {
+      message: "スクーリングの有無を選択してください。",
+    })
+    .refine((val) => ["true", "false"].includes(val), {
+      message: "スクーリングの有無を正しく選択してください。",
+    }),
+  movingOutsideThePrefecture: z
+    .string()
+    .refine((val) => val !== "", {
+      message: "県外移動の有無を選択してください。",
+    })
+    .refine((val) => ["true", "false"].includes(val), {
+      message: "県外移動の有無を正しく選択してください。",
+    }),
+  commutingStyle: z
+    .string()
+    .refine((val) => val !== "", { message: "通学形態を選択してください。" })
+    .refine((val) => ["通学", "オンライン"].includes(val), {
+      message: "通学形態を正しく選択してください。",
+    }),
+  highSchool: z
+    .string()
+    .refine((val) => val !== "", { message: "学校の種類を選択してください。" })
+    .refine((val) => ["通信制高等学校", "サポート校"].includes(val), {
+      message: "学校の種類を正しく選択してください。",
+    }),
+  attendanceFrequency: z
+    .string()
+    .refine((val) => val !== "", { message: "登校頻度を選択してください。" })
+    .refine(
+      (val) =>
+        ["週1", "週2", "週3", "週4", "週5", "オンライン", "自由"].includes(val),
+      { message: "登校頻度を正しく選択してください。" }
+    ),
 });
 
 type FormValues = z.infer<typeof formSchema>;
@@ -56,11 +74,11 @@ const Form = () => {
       initialSetupCosts: 0, // 初期値設定
       tuitionFee: 0,
       testFee: 0,
-      schooling: undefined, // 初期値をundefinedに設定
-      movingOutsideThePrefecture: undefined,
-      commutingStyle: undefined,
-      highSchool: undefined,
-      attendanceFrequency: undefined,
+      schooling: "",
+      movingOutsideThePrefecture: "",
+      commutingStyle: "",
+      highSchool: "",
+      attendanceFrequency: "",
       // fireStoreのコレクションを追加
     },
   });
