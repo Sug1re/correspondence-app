fb815a476348792382ee7e1ae5ae0d93df1f29ab
Sug1re <sugirex@icloud.com>
2025-03-19 15:53:39 +0900

    buildエラー解消

--- components/component/SchoolCard.tsx
+++ components/component/SchoolCard.tsx
@@ -1,43 +1,43 @@
-"use client";
+// "use client";
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-// fireStore の型定義
-type School = {
-  id: string;
-  name: string;
-  course: string;
-  totalTuitionFee: number;
-  firstYearFee: number;
-  secondYearFee: number;
-  thirdYearFee: number;
-  testFee: number;
-  movingOutsideThePrefecture: boolean;
-  commutingStyle: string;
-  highSchool: string;
-  attendanceFrequency: string[];
-  // fireStoreのコレクションを追加
-};
+// // fireStore の型定義
+// type School = {
+//   id: string;
+//   name: string;
+//   course: string;
+//   totalTuitionFee: number;
+//   firstYearFee: number;
+//   secondYearFee: number;
+//   thirdYearFee: number;
+//   testFee: number;
+//   movingOutsideThePrefecture: boolean;
+//   commutingStyle: string;
+//   highSchool: string;
+//   attendanceFrequency: string[];
+//   // fireStoreのコレクションを追加
+// };
 
-// モーダルのUI
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 330,
-  bgcolor: "background.paper",
-  px: 4,
-  py: 2,
-  borderRadius: 3,
-  border: `2px solid #FF6600`,
-};
+// // モーダルのUI
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 330,
+//   bgcolor: "background.paper",
+//   px: 4,
+//   py: 2,
+//   borderRadius: 3,
+//   border: `2px solid #FF6600`,
+// };
 
-const SchoolCard = () => {
-  const [openModalId, setOpenModalId] = useState<string | null>(null); // 各学校ごとのモーダルのIDを管理
-  const handleOpen = (schoolId: string) => setOpenModalId(schoolId); // モーダルを開く関数
-  const handleClose = () => setOpenModalId(null); // モーダルを閉じる関数
-  return <></>;
-};
+// const SchoolCard = () => {
+//   const [openModalId, setOpenModalId] = useState<string | null>(null); // 各学校ごとのモーダルのIDを管理
+//   const handleOpen = (schoolId: string) => setOpenModalId(schoolId); // モーダルを開く関数
+//   const handleClose = () => setOpenModalId(null); // モーダルを閉じる関数
+//   return <></>;
+// };
 
-export default SchoolCard;
+// export default SchoolCard;
