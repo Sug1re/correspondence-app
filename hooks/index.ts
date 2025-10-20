82da6ff0146b590567a857fffa5f0f0ac7ac80ae
Sug1re <sugirex@icloud.com>
2025-10-20 16:43:49 +0900

    remove:hooks/index

--- hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import useHeaderBar from "./useHeaderBar";
-import useModal from "./useModal";
-import usePagination from "./usePagination";
-import useSearchSchoolParams from "./useSearchSchoolParams";
-
-export { useHeaderBar,useModal,usePagination,useSearchSchoolParams};
