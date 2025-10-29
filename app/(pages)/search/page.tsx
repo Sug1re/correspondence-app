3b95c39f7c650aa224a5daf796d77aa1cc08bbf4
Sug1re <sugirex@icloud.com>
2025-10-30 04:46:17 +0900

    fix:npm run build[修正]

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Stack } from "@mui/material";
+import React, { Suspense } from "react";
+import { Box, CircularProgress, Container, Stack } from "@mui/material";
 import Header from "@/components/Header";
 import ScrollToTopButton from "@/components/Buttons/ScrollTopButton";
 import { SearchSection } from "@/components/Sections/SearchSection";
@@ -11,7 +11,15 @@ export default function SearchPage() {
 
       <Container maxWidth="md">
         <Stack spacing={4} sx={{ mt: 4 }}>
-          <SearchSection />
+          <Suspense
+            fallback={
+              <Box sx={{ display: "flex", justifyContent: "center", py: 4 }}>
+                <CircularProgress />
+              </Box>
+            }
+          >
+            <SearchSection />
+          </Suspense>
         </Stack>
       </Container>
 
